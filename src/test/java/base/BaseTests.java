8f74a6ac26247e55b0710e2f9d9ae597d6ac85fe
Badr-1 <badrmohamedashraf@gmail.com>
2023-05-12 19:45:31 +0300

    🧑‍💻 Chapter 12 Taking Screenshots

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,18 +1,27 @@
 package base;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 import utils.WindowManager;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class BaseTests {
     private WebDriver driver;
     protected HomePage homePage;
+
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         goHome();
@@ -20,12 +29,23 @@ public class BaseTests {
     }
 
     @BeforeMethod
-    public void goHome(){
+    public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
     }
+
     @AfterClass
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            Files.move(screenshot.toPath(), new File("resources/screenshots/" + result.getName() + ".png").toPath());
+            System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+        }
     }
 
     public static void main(String[] args) {
@@ -33,7 +53,7 @@ public class BaseTests {
         test.setUp();
     }
 
-    public WindowManager getWindowManager(){
+    public WindowManager getWindowManager() {
         return new WindowManager(driver);
     }
 }
